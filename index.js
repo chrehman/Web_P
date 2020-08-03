41a850625f7b3cce5417cfbae2363c4a43a19ff4
abdulrehman <chrehman1998@gmail.com>
2020-08-03 11:31:27 +0500

    adding index.js for hadith

---
+++ index.js
@@ -0,0 +1,24 @@
+$(".bukhari").click(function(event){
+   
+    $("#buk").toggle(1000);
+   
+
+});
+$(".muslim").click(function(event){
+   
+    $("#mus").toggle(1000);
+   
+
+});
+$(".tirmizi").click(function(event){
+   
+    $("#tir").toggle(1000);
+   
+
+});
+$(".majah").click(function(event){
+   
+    $("#maj").toggle(1000);
+   
+
+});
