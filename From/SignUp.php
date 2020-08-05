24b0492e0c241550c0e03337aa525aa8c588df73
abdulrehman <chrehman1998@gmail.com>
2020-08-05 07:28:07 +0500

    index

--- From/SignUp.php
+++ From/SignUp.php
@@ -198,7 +198,7 @@ function test_input($data){
                         </form>
                     </div>
                     <div class="signup-image">
-                        <figure><img src="images/signup-image.jpg" alt="sing up image"></figure>
+                        <figure><img src="images/pic1.jpg" alt="sing up image"></figure>
                         <a href="./SignIn.php" class="signup-image-link">I am already member</a>
                     </div>
                 </div>
